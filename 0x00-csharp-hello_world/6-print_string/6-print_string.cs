46718fa96a635f23b93829e1b9fd3a95e35a9a4d
Chris Vanndy <2736@holbertonschool.com>
2021-07-12 16:49:32 -0500

    output error

--- 0x00-csharp-hello_world/6-print_string/6-print_string.cs
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -5,7 +5,7 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "Holberton School";
-		Console.WriteLine("{0} {1} {2}", str, str, str);
+		Console.WriteLine("{0}{1}{2}", str, str, str);
 		Console.WriteLine("{0}", str.Substring(0, 9));
         }
 }
