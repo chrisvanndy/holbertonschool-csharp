bd6189360e9d9129343a48f3c78de9396367fe7c
Chris Vanndy <2736@holbertonschool.com>
2021-07-13 18:16:46 -0500

    define 2 var, nested loop,  _ \, handle newlines

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            for (int x = 0; x < length; x++)
+            {
+                for (int y = 0; y < x; y++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("\\");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
