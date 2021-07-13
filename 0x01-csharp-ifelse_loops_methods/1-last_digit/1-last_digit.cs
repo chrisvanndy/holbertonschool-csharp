b125ad36d6a55d5199e11c0f9c048086584c5831
Chris Vanndy <2736@holbertonschool.com>
2021-07-13 18:42:48 -0500

    all day to find an extra "is 0" in code......

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -7,16 +7,16 @@ class Program
 	Random rndm = new Random();
 	int number = rndm.Next(-1000, 1000);
 	if (number % 10 > 5)
-        { 
+    { 
 	    Console.WriteLine("The last digit of {0:d} is {1:d} and is greater than 5", number, (number % 10));
-	    }
-        else if (number % 10 == 0)
-        {
-	    Console.WriteLine("The last digit of {0:d} is {1:d} is 0 and is 0", number, (number % 10));
-        }
-        else
-        {
+	}
+    else if (number % 10 == 0)
+    {
+	    Console.WriteLine("The last digit of {0:d} is {1:d} and is 0", number, (number % 10));
+    }
+    else
+    {
         Console.WriteLine("The last digit of {0:d} is {1:d} and is less than 6 and not 0", number, (number % 10));
-	    }
+	}
     }
 }
