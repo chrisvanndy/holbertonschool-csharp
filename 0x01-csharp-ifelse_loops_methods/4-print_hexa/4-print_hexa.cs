46ff1cb79e28720a9ef8b0d3fb10b65a5e87d0fa
Chris Vanndy <2736@holbertonschool.com>
2021-07-13 13:55:58 -0500

    Console.Write()

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -7,7 +7,7 @@ class Program
         int hex;
         for (hex = 0; hex <= 98; hex++)
         {
-            Console.WriteLine("{0} = 0x{0:x2}", hex);
+            Console.Write("{0:d} = 0x{0:x}\n", hex);
         }
     }
 }
