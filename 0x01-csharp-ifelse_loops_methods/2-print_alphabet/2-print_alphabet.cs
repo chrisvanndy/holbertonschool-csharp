0e6465906277ed37ae29e1567ab2a8310d3f99ca
Chris Vanndy <2736@holbertonschool.com>
2021-07-13 11:19:33 -0500

    print lower alpha NO newline

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char alpha;
+            for (alpha = 'a'; alpha <= 'z'; alpha++)
+	    {
+                Console.Write("{0}", alpha);
+            }	
+	}
+    }
+}
