00b0a284add19e11f05ea293e55c2cbbd39bfc61
Chris Vanndy <2736@holbertonschool.com>
2021-07-13 13:16:12 -0500

    syntax

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -12,11 +12,11 @@ class Program
 	}
         else if (number % 10 < 6 && number % 10 != 0)
         {
-	    Console.WriteLine("The last digit of {0:D} is {1:D} and is less than 6 and not 0", number, number % 10);
+	    Console.WriteLine("The last digit of {0:D} is {1:D} and is less than 6 and not 0", number, (number % 10));
 	}
         else 
         {
-	    Console.WriteLine("The last digit of {0} is {1} is 0 and is 0", number, number % 10);
+	    Console.WriteLine("The last digit of {0} is {1} is 0 and is 0", number, (number % 10));
         } 
     }
 }
