10cdaeb19754d346dfa7968e18a36830728cfec1
Chris Vanndy <2736@holbertonschool.com>
2021-07-13 12:05:44 -0500

    if e or q continue

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char alpha;
+            for (alpha = 'a'; alpha <= 'z'; alpha++)
+            {
+                if (alpha == 'e'|| alpha == 'q')
+                    continue;
+                else
+                    Console.Write("{0}", alpha);
+            }
+        }
+    }
+}
