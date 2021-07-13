2873e63e0765627509d096f9193150ac147dc2a7
Chris Vanndy <2736@holbertonschool.com>
2021-07-13 12:08:32 -0500

    syntax error // 0x{0:X2}

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hex;
+            for (hex = 0; hex <= 98; hex++)
+            {
+                Console.WriteLine("{0} = 0x{0:X2}", hex);
+            }
+	}
+    }
+}
