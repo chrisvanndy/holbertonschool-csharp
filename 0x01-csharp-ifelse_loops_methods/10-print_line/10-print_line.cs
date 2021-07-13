fb7b934119d68521449d7b9c7470997a4392f78c
Chris Vanndy <2736@holbertonschool.com>
2021-07-13 17:54:26 -0500

    typo in filename

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        int i;
+        if (length <= 0)
+        {
+            Console.WriteLine("\n");
+        }
+        else
+        {
+            for (i = 0; i <= length; i++)
+            {
+                Console.Write("_");
+            }
+            Console.Write("\n");
+        }
+    }
+}
