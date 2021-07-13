b996fb6b15c8fe49561503ac8c69d2bbe6b50854
Chris Vanndy <2736@holbertonschool.com>
2021-07-13 13:12:05 -0500

    removing additional variable

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -6,18 +6,17 @@ class Program
     {
 	Random rndm = new Random();
 	int number = rndm.Next(-1000, 1000);
-	int last = number % 10;
-	if (last > 5)
+	if (number % 10 > 5)
         { 
-	    Console.WriteLine("The last digit of {0} is {1} and is greater than 5", number, last);
+	    Console.WriteLine("The last digit of {0:D} is {1:D} and is greater than 5", number, (number % 10));
 	}
-        else if (last < 6 && last != 0)
+        else if (number % 10 < 6 && number % 10 != 0)
         {
-	    Console.WriteLine("The last digit of {0} is {1} and is less than 6 and not 0", number, last);
+	    Console.WriteLine("The last digit of {0:D} is {1:D} and is less than 6 and not 0", number, number % 10);
 	}
         else 
         {
-	    Console.WriteLine("The last digit of {0} is {1} is 0 and is 0", number, last);
+	    Console.WriteLine("The last digit of {0} is {1} is 0 and is 0", number, number % 10);
         } 
     }
 }
