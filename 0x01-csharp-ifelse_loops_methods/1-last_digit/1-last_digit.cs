e7aaad38d479a7251f759b8d31bb5730aacbe6f8
Chris Vanndy <2736@holbertonschool.com>
2021-07-13 18:38:09 -0500

    efficientcy check

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -8,15 +8,15 @@ class Program
 	int number = rndm.Next(-1000, 1000);
 	if (number % 10 > 5)
         { 
-	    Console.WriteLine("The last digit of {0:D} is {1:D} and is greater than 5", number, (number % 10));
-	}
-        else if (number % 10 < 6 && number % 10 != 0)
+	    Console.WriteLine("The last digit of {0:d} is {1:d} and is greater than 5", number, (number % 10));
+	    }
+        else if (number % 10 == 0)
         {
-	    Console.WriteLine("The last digit of {0:D} is {1:D} and is less than 6 and not 0", number, (number % 10));
-	}
-        else 
+	    Console.WriteLine("The last digit of {0:d} is {1:d} is 0 and is 0", number, (number % 10));
+        }
+        else
         {
-	    Console.WriteLine("The last digit of {0} is {1} is 0 and is 0", number, (number % 10));
-        } 
+        Console.WriteLine("The last digit of {0:d} is {1:d} and is less than 6 and not 0", number, (number % 10));
+	    }
     }
 }
