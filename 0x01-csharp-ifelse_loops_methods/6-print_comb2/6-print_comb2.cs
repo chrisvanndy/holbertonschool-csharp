334414bf0bb892fcf0fad2994972a15f531f4f3f
Chris Vanndy <2736@holbertonschool.com>
2021-07-13 16:03:08 -0500

    Adding task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{ 
+    static void Main(string[] args)
+    {
+        for (int x = 0; x < 9; x++)
+        {
+            for (int y = (x + 1); y < 10; y++)
+            {
+                if (x < 8)
+                {
+                    Console.Write("{0}{1}, ", x, y);
+                }
+                else
+                {
+                    Console.WriteLine("{0}{1}", x, y);
+                }
+            }
+        }
+    }
+}
