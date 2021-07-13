d85beaaef4d0f6f6c757828fe42cf6525957f011
Chris Vanndy <2736@holbertonschool.com>
2021-07-13 14:22:39 -0500

    list em

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+	int num;
+	for (num = 0; num <= 99; num++)
+        {
+            if (num <= 98)
+            {
+                Console.Write("{0}, ", num);
+            }
+            else
+            {
+                Console.Write("{0}", num);
+            }
+        }
+    }
+}
