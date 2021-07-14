ae63d67a451c56f3c16abdf776a277890b25b7b7
Chris Vanndy <2736@holbertonschool.com>
2021-07-14 14:43:01 -0500

    sorted list, return count -1

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+
+        if (myList.Count < 1 || myList == null)
+        {
+            Console.WriteLine("List is Empty");
+            return -1;
+        }
+        // sorting list sorts items in ascending order
+        myList.Sort();
+        // return sorted list (count - 1)
+        return (myList[myList.Count -1]);
+    
+    }    
+}
+
