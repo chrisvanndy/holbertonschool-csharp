160ef42b00c9aa897c7ef86e4ef22c490c3bcf73
Chris Vanndy <2736@holbertonschool.com>
2021-07-14 16:22:45 -0500

    count var, iterate over k,v pairs return count

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int i = 0;
+        foreach (KeyValuePair<string, string> pairs in myDict)
+        {
+            i++;
+        }
+        return i;
+    }
+}
+
