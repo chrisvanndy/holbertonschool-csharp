c2c7a81c5351cfe06e11f04e46d3d6b6c49eac4e
Chris Vanndy <2736@holbertonschool.com>
2021-07-14 15:31:45 -0500

    myList.Remove(index);

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-deleted_at.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        myList.Remove(index);
+        return myList;
+    }
+}
+
