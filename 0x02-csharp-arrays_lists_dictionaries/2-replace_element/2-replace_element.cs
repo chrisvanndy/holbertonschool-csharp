f6c141521c6816264fd025208b63b523284a6796
Chris Vanndy <2736@holbertonschool.com>
2021-07-14 13:08:36 -0500

    parameters fixed.

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -5,7 +5,15 @@ class Array
 {
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
-       array[index] = n;
-       return array;
+        if (array.Length <= index || index < 0)
+        {
+            Console.WriteLine("Index out of range");
+            return array;
+        }
+        else
+        {
+            array[index] = n;
+            return array;
+        }
     }
 }
