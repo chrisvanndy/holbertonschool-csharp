3551a702583ada597749f926ddf2889415634c3d
Chris Vanndy <2736@holbertonschool.com>
2021-07-14 12:51:28 -0500

    return array at index

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+class Array
+{
+       public static int elementAt(int[] array, int index)
+       {
+           // index cannot be length (0 index) or negative
+           if (array.Length <= index || index < 0)
+           {
+               Console.WriteLine("Index out of range");
+               return -1;
+           }
+           // simple return array at given index
+           return array[index];
+            
+       }    
+}
+
