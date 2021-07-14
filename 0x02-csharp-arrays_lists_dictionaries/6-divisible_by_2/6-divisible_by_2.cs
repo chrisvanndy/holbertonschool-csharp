9cae7229b9e63f4912ffce7851a992540d88cde2
Chris Vanndy <2736@holbertonschool.com>
2021-07-14 15:13:12 -0500

    bool list populated with t /f

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        // return a new list
+        List<bool> newList = new List<bool>();
+
+        foreach (int num in myList)
+        {
+            if (num % 2 == 0)
+            {
+                newList.Add(true);
+            }
+            else
+            {
+                newList.Add(false);
+            }
+        }
+        
+        return newList;
+    }
+}
+
