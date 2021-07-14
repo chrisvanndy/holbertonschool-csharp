02c7d3c03367bd85dc5f7fa11dd0d8b66838220a
Chris Vanndy <2736@holbertonschool.com>
2021-07-14 14:17:44 -0500

    notes added

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        // defining a lis must be have (), {}, []
+        var items = new List<int> {};
+        for (int i = 0; i < size; i++)
+        {
+            // add items to a list in this way
+            items.Add(i);
+            // print index value
+            Console.Write(items[i]);
+            if (i < size - 1)
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+        return items;
+        
+
+
+    }
+}
+
