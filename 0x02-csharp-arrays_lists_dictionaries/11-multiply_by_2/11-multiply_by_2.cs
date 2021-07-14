fa040850d13940102f11130b3349a61a125f4ac2
Chris Vanndy <2736@holbertonschool.com>
2021-07-14 17:41:49 -0500

    iterate through passed dict, double new dict

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        // new dictionary = myDict
+        Dictionary<string, int> newest = new Dictionary<string, int>(myDict);
+
+        // iterating through passed dict
+        foreach (string key in myDict.Keys)
+        {
+            // for each key in myDict newest key = *2
+            newest[key] = (newest[key] * 2);
+        }
+
+        return newest;
+    }
+}
+
