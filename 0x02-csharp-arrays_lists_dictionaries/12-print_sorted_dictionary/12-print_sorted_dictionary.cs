9fa9db0ab7d7e5966ec791c32748bab7f168657a
Chris Vanndy <2736@holbertonschool.com>
2021-07-14 18:11:24 -0500

    dict to list, sort list, print sorted list + value

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+         // Step 2: get keys and sort them.
+        var list = myDict.Keys.ToList();
+        list.Sort();
+
+        // Step 3: loop through keys.
+        foreach (var key in list)
+        {
+            Console.WriteLine("{0}: {1}", key, myDict[key]);
+        }
+    }
+}
+
