097ebe26c612288a9964c729a75d86d905a22579
Chris Vanndy <2736@holbertonschool.com>
2021-07-15 14:24:01 -0500

    addfirst stored in LLN var = newnode return

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> newnode = myLList.AddFirst(n);
+        return newnode;
+    }
+}
