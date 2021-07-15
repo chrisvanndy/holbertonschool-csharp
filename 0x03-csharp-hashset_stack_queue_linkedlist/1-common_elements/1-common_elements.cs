0cb1c9b33beb215db1473dc87b4a934cbc31eed3
Chris Vanndy <2736@holbertonschool.com>
2021-07-15 10:13:35 -0500

    hastables intersect, into new list, return it

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> inBoth1 = new HashSet<int>(list1);
+        HashSet<int> inBoth2 = new HashSet<int>(list2);
+        List<int> newList = new List<int>();
+
+        inBoth1.IntersectWith(inBoth2);
+
+        foreach (int number in inBoth1)
+        {
+            newList.Add(number);
+        }
+        return newList;
+    }
+
+    
+}
+
