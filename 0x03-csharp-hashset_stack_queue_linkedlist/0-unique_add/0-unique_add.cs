47aec0e76e97463c4d846b2fcedaafc110664511
Chris Vanndy <2736@holbertonschool.com>
2021-07-15 09:38:54 -0500

    first commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        List<int> math = new List<int>();
+        int sum = 0;
+        foreach (int number in myList)
+        {
+            if(!math.Contains(number))
+            {
+                math.Add(number);
+            }
+        }
+        
+        for (int i = 0; i < math.Count; i++)
+        {
+            sum += math[i];
+        }
+        return sum;
+    }
+}
+
