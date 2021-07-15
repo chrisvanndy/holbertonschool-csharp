db810259e5d430a5d2d14287578bbde54c1c264c
Chris Vanndy <2736@holbertonschool.com>
2021-07-14 19:57:44 -0500

    not finished

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int[,] newmatrix = new int[myMatrix];
+        
+        for (int x = 0; x < myMatrix.Length)
+    }
+}
+
