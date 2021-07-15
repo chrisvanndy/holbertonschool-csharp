b7e4622664dfdd3d50ec4a65645b50d9ed1d1d7c
Chris Vanndy <2736@holbertonschool.com>
2021-07-15 15:03:39 -0500

    foreach node if node = value // ret -1 if len

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int index = -1;
+
+        foreach (int node in myLList)
+        {
+            index += 1;
+            if (node == value)
+            {
+                break;
+            }
+            else if (index == myLList.Count - 1)
+            {
+                return -1;
+            }
+        }
+        return index;
+    }
+}
+
