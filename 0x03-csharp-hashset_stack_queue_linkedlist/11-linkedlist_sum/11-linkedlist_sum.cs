e94ad3069cb7ca988fb73fe9507a979cffc5b8c6
Chris Vanndy <2736@holbertonschool.com>
2021-07-15 18:02:44 -0500

    var += data if index break else + return

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int math = 0;
+        int index = 0;
+
+        foreach (int number in myLList)
+        {
+            
+            if (index == myLList.Count) 
+            {   
+                break;
+            }
+            else
+            {
+                math += number;
+                index++;
+            }
+        }
+        return math;
+    }
+}
+
