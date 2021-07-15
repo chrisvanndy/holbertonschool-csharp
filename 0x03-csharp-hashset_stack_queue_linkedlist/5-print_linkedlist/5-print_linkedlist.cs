f356a93b35933e2f690c0678784a1d43641944f0
Chris Vanndy <2736@holbertonschool.com>
2021-07-15 13:11:38 -0500

    loop, addlast, print return

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> myLList = new LinkedList<int>();
+
+        for (int i = 0; i < size; i++)
+        {
+            myLList.AddLast(i);
+            Console.WriteLine(i);
+        }
+
+        return myLList;
+    }
+}
+
+        
