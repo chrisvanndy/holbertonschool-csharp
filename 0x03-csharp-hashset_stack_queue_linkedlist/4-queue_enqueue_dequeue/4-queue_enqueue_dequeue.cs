3f73953b83ac4222107165064eee44a27c5377b0
Chris Vanndy <2736@holbertonschool.com>
2021-07-15 12:08:33 -0500

    string val = Q.Dequeue() if search = val X

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)         
+    {
+        string booly;
+
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+
+        if (aQueue == null || aQueue.Count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        else
+        {
+            Console.WriteLine("First itemn: {0}", aQueue.Peek());
+        }
+
+        aQueue.Enqueue(newItem);
+
+        if (aQueue.Contains(search))
+        {
+            booly = "True";
+            while (aQueue.Count > 0)
+            {
+                string val = aQueue.Dequeue();
+                if (val == search)
+                {
+                    break;
+                }
+            }
+        }
+        else
+        {
+            booly = "False";
+        }
+
+        Console.WriteLine("Queue contains {0}: {1}", search, booly);
+
+        return aQueue;        
+    }
+}
+
