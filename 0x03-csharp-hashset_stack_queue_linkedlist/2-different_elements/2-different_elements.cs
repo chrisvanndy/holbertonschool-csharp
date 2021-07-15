ea089329bb2dfb70c06c5021253c2248b4478e5f
Chris Vanndy <2736@holbertonschool.com>
2021-07-15 10:28:46 -0500

    symetrical hashsets into newlist and sorted

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)    
+    {
+        HashSet<int> diff1 = new HashSet<int>(list1);
+        HashSet<int> diff2 = new HashSet<int>(list2);
+        List<int> newList = new List<int>();
+
+        diff1.SymmetricExceptWith(diff2);
+
+        foreach (int number in diff1)
+        {
+            newList.Add(number);
+            newList.Sort();
+        }
+        return newList;
+
+    }
+}
+
