28988bfc97508368ca6f5e07f131e50909d98d00
Chris Vanndy <2736@holbertonschool.com>
2021-07-15 17:47:10 -0500

    return number / return 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int count = 0;
+        
+        foreach (int number in myLList)
+        {
+            if (count == n)
+            {   
+                return number;
+            }
+            count++;
+        }
+        return 0;
+    }
+}
+
