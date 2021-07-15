74c04187b39aede777671ff555af75b965380765
Chris Vanndy <2736@holbertonschool.com>
2021-07-15 11:42:58 -0500

    pop() returns current value if search break

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        int count = 0;
+        string booly;
+
+        
+        if (aStack == null || aStack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            foreach (string item in aStack)
+            {
+                count++;
+            }
+        }  
+
+        Console.WriteLine("Number of items: {0}", count);
+
+        Console.WriteLine("Top item: {0}", aStack.Peek());
+
+        if (aStack.Contains(search))
+        {
+            booly = "True";
+            while (aStack.Count > 0)
+            {
+                aStack.Pop();
+                if (aStack.Pop() == search)
+                {
+                    break;
+                }
+               
+            }
+        }
+        else
+        {
+            booly = "False";
+        }
+        Console.WriteLine("Stack contains {0}: {1}", search, booly);
+
+        aStack.Push(newItem);
+
+        return aStack;
+
+
+    }
+}
