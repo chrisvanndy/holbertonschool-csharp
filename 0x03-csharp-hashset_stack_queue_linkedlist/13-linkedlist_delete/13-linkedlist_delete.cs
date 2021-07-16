836df834eaca3737835c06388ae4159686dcc577
Chris Vanndy <2736@holbertonschool.com>
2021-07-16 11:41:18 -0500

    edge cases / myLList.Remove(current)

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int i = 0;
+        LinkedListNode<int> current;
+
+        if (myLList == null || myLList.Count == 0)
+            return;
+        if (myLList.Count == 0)
+            myLList.RemoveFirst();
+        else if (index == myLList.Count -1)
+            myLList.RemoveLast();
+        else
+        {
+            for (current = myLList.First; current != null; current = current.Next)
+            {
+                if (i == index)
+                {
+                    myLList.Remove(current);
+                    break;
+                }
+                i++;
+            }
+        }
+    }
+}
+
