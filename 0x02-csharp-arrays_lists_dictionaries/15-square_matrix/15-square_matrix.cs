200182ee85ee99349c27fa1c241820fd1495b9c3
Chris Vanndy <2736@holbertonschool.com>
2021-07-16 11:02:21 -0500

    use GetLenght(0) and GetLenght(1)

--- 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-class Program
+class Matrix
 {
     public static int[,] Square(int[,] myMatrix)
     {
-        int[,] newmatrix = new int[myMatrix];
         
-        for (int x = 0; x < myMatrix.Length)
+        int rows = myMatrix.GetLength(0);
+        int columns = myMatrix.GetLength(1);
+
+        for (int x = 0; x < rows; x++)
+        {
+            for (int y = 0; y < columns; y++)
+            {
+                myMatrix[x,y] *= myMatrix[x,y];
+            }
+        }
+        return myMatrix;
     }
 }
 
