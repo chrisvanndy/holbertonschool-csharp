112be9569a77a12f198ec743965466196987f9cb
Chris Vanndy <2736@holbertonschool.com>
2021-07-16 11:11:10 -0500

    definte current  node instance / itterate index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    { 
+        LinkedListNode<int> current = myLList.First;
+
+        foreach (int numbers in myLList)
+        {
+            if (n < numbers)
+            {
+                myLList.AddBefore(current, n);
+                return myLList.Find(n);
+            }
+            current = current.Next;
+        }
+        myLList.AddLast(n);
+        return myLList.Last;
+    }
+}
