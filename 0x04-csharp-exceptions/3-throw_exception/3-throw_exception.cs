282fedf987607691ecef05f15107641bf185c64a
Chris Vanndy <2736@holbertonschool.com>
2021-07-19 12:28:20 -0500

    define and throw new exception

---
+++ 0x04-csharp-exceptions/3-throw_exception/3-throw_exception.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+class Except
+{
+    public static void Throw()
+    {
+        Exception newException = new Exception("Defined new exception for task 3");
+        throw newException;
+    }
+}
+
