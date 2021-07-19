23f38575f957c2de92a2a19bf39423177c5cd79d
Chris Vanndy <2736@holbertonschool.com>
2021-07-19 10:38:45 -0500

    try division, catch 0, finally print

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int num = 0;
+        try
+        {
+            num = (a / b);
+        }
+        catch
+        { 
+            if (b == 0)
+                Console.WriteLine("Cannot divide by zero");
+                    
+
+        }
+        finally
+        {
+            Console.WriteLine("{0} / {1} = {2}", a, b, num);
+        }
+    }
+}
+
