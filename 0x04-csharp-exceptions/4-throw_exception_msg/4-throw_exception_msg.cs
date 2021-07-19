db3e07b406f2a32118ed3c8099130d4e85cfedc3
Chris Vanndy <2736@holbertonschool.com>
2021-07-19 12:36:09 -0500

    msg to var, pass var to new exception, throw

---
+++ 0x04-csharp-exceptions/4-throw_exception_msg/4-throw_exception_msg.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+class Except
+{
+    public static void ThrowMsg(string msg)
+    {
+        string myStr = msg;
+        Exception newExcept = new Exception(msg);
+        throw newExcept;
+    }
+}
+
