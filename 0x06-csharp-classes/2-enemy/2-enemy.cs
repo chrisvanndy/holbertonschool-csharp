babfdb6f3efe12ab4e5f7e3fae9bb81c4f100eff
Chris Vanndy <2736@holbertonschool.com>
2021-07-21 10:00:36 -0500

    if val == "" throw new AE, else set health

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents the Zombie class</summary>
+    public class Zombie
+    {
+        /// <summary>Sets the value for health.</summary>
+        public int health;
+
+       /// <summary>Initializes a new instance of Zombie class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary>Initializes a new instance of Zombie class with passed int variable.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            
+            health = value;
+        }
+            
+     }
+}
+
