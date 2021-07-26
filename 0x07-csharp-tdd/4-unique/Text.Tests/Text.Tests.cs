7ad59bc77c85d567550fef516c01a7f0e83fbfd3
Chris Vanndy <2736@holbertonschool.com>
2021-07-26 13:34:06 -0500

    fixed tests

--- 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -13,5 +13,37 @@ namespace Text.Tests
             int index = Str.UniqueChar("Programmer");
             Assert.AreEqual(index, 0);
         }
+
+        [Test]
+        ///<summary>Returns index of first nonrepeating characther last</summary>
+        public void Returns_nonrepeat_last()
+        {
+            int index = Str.UniqueChar("Ppeeddr");
+            Assert.AreEqual(index, 0);
+        }
+
+        [Test]
+        ///<summary>Returns index of first nonrepeating characther middle</summary>
+        public void Returns_nonrepeat_middle()
+        {
+            int index = Str.UniqueChar("Ppeedlltt");
+            Assert.AreEqual(index, 0);
+        }
+
+        [Test]
+        ///<summary>Returns index of first nonrepeating characther (none)</summary>
+        public void Returns_nonrepeat_none()
+        {
+            int index = Str.UniqueChar("ppssttllmmooaa");
+            Assert.AreEqual(index, -1);
+        }
+
+        [Test]
+        ///<summary>Returns index of first nonrepeating characther (empty)</summary>
+        public void Returns_nonrepeat_empty()
+        {
+            int index = Str.UniqueChar("");
+            Assert.AreEqual(index, -1);
+        }
     }
 }
