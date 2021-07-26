93e1126acea9b6ea1e499a394a89bc2a44fcb0ce
Chris Vanndy <2736@holbertonschool.com>
2021-07-26 09:16:16 -0500

    adding unit tests for null / 0

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -12,11 +12,32 @@ namespace MyMath.Tests
         public static int[,] testMatrix = new int[3,3] {{2, 4, 6}, {8, 10, 12}, {14, 16, 18}};
         
         [Test]
-        ///<summary>Text Divide function with dividision by "num".</summary>
+        ///<summary>Test Divide function with dividision by "num".</summary>
         public void Matrix_Division()
         {
             int[,] solution = Matrix.Divide(testMatrix, 2);
             Assert.AreEqual(solution, new int[3,3]{{1, 2, 3,}, {4, 5, 6}, {7, 8, 9}});
+        }
+
+        [Test]
+        ///<summary>Test Divide function with division by 0.</summary>
+        public void Matrix_Divide_0()
+        {
+            try
+            {
+                int[,] soulution = Matrix.Divide(testMatrix, 0);
+            }
+            catch(Exception e)
+            {
+                Assert.AreEqual(e, "Num cannot be 0");
+            }
+        }
+        [Test]
+        ///<summary>Test Divide function with null matrix.</summary>
+        public void Matrix_null()
+        {
+            int[,] solution = Matrix.Divide(null, 2);
+            Assert.AreEqual(solution, null);
         }
 
     }
