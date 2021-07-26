47d2932230d8c2f37c22f5d8d2e9651b80bcd4fb
Chris Vanndy <2736@holbertonschool.com>
2021-07-26 09:40:15 -0500

    Pushing first test

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -1,18 +1,17 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace MyMath.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
+       
         [Test]
-        public void Test1()
+        ///<summary>Max_Positive_ints checks for maximum int among all positive ints<summary>
+        public void Max_Positive_ints()
         {
-            Assert.Pass();
+             var myList = new List<int>{1, 2, 3, 4, 5};
+             Assert.AreEqual(5, Operations.Max(myList));
         }
     }
 }
--- 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -1,20 +1,29 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace MyMath
 {
     ///<summary>Operations defines functions in the Operations class</summary>
     public class Operations
     {
-        ///<sumary>Max method returns max int in a given list</summary>
+        ///<summary>Max method returns max int in a given list</summary>
+        ///<param name="nums">List nums to be analyized for max member</param>
         public static int Max(List<int> nums)
         {
-            if (nums.GetLength() == 0)
+            if (nums.Count == 0)
             {
                 return 0;
             }
-            int max = nums.Max();
-            return max;
+    
+           int maximum = nums[0];
+           foreach (int numbers in nums)
+           {
+               if (numbers > maximum)
+               {
+                   maximum = numbers;
+               }
+           }
+           return maximum;
         }
     }
 }
