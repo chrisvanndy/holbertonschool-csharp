3726e16a982200af01bdc96612845c35e3caf874
Chris Vanndy <2736@holbertonschool.com>
2021-07-26 10:13:13 -0500

    adding tests

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -7,11 +7,39 @@ namespace MyMath.Tests
     {
        
         [Test]
-        ///<summary>Max_Positive_ints checks for maximum int among all positive ints<summary>
-        public void Max_Positive_ints()
+        ///<summary>Max_Positive_ints checks for maximum int at end of list<summary>
+        public void Max_Positive_end()
         {
              var myList = new List<int>{1, 2, 3, 4, 5};
              Assert.AreEqual(5, Operations.Max(myList));
+        }
+        [Test]
+        ///<summary>Max_Positive_end checks for maximum int at begginning of list<summary>
+        public void Max_Positive_begin()
+        {
+            var myList = new List<int>{5, 4, 3, 2, 1};
+            Assert.AreEqual(5, Operations.Max(myList));
+        }
+         [Test]
+        ///<summary>Max_Positive_end checks for maximum int in middle of list<summary>
+        public void Max_Positive_middle()
+        {
+            var myList = new List<int>{1, 2, 5, 3, 4};
+            Assert.AreEqual(5, Operations.Max(myList));
+        }
+        [Test]
+        ///<summary>Max_Positive_end checks for maximum int with negatives in list<summary>
+        public void Max_Positive_negative()
+        {
+            var myList = new List<int>{1, -2, 5, -3, 4};
+            Assert.AreEqual(5, Operations.Max(myList));
+        }
+        [Test]
+        ///<summary>Max_Positive_end checks for maximum int an empty list<summary>
+        public void Max_Positive_empty()
+        {
+            var myList = new List<int>{};
+            Assert.AreEqual(0, Operations.Max(myList));
         }
     }
 }
