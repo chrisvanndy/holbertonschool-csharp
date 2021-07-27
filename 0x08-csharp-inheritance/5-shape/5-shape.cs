0f6c58d0114b3822c40e6c06e7638db5f7afd929
Chris Vanndy <2736@holbertonschool.com>
2021-07-27 12:48:54 -0500

    throw new Exception

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+///<summary>Shape class defines methods to work with shapes</summary>
+class Shape
+{
+    ///<summary>Area() defines method to return area of given shape.</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
