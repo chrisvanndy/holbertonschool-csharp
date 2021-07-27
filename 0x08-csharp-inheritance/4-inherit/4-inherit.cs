ada90d630f4bf509cd4a6dde4f13d2288b2b7f8d
Chris Vanndy <2736@holbertonschool.com>
2021-07-27 12:40:32 -0500

    Animal - base Dog - subclass

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+    ///<summary>Animal class defines properties shared by all sublcasses of animal</summary>
+    class Animal
+    {
+    
+    }
+    ///<summary>Dog inherits from class Animal</summary>
+    class Dog : Animal
+    {
+
+    }
+
