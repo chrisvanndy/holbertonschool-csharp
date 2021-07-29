1c53b595ad5f8e3c5c5a85f45405d788c8d3d441
Chris Vanndy <2736@holbertonschool.com>
2021-07-29 12:09:44 -0500

    learning to use System.Math for vector magnitudes

---
+++ 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+///<summary>VectorMath contains functions to calculate the magnitue of 2D and 3D vectors.</summary>
+class VectorMath
+{
+    ///<summary>Magnitude caclulated the lenght of a vector</summary>
+    ///<param name="vector">The given coordinates of a 2D or 3C vector</param>
+    public static double Magnitude(double[] vector)
+    {
+        double x = 0;
+
+        if (vector.Length < 2 || vector.Length > 3)
+        {
+            return -1;
+        }
+
+        foreach (double c in vector)
+        {
+            double sqr = 0;
+            sqr = Math.Pow(c, 2);
+            x += sqr;
+        }
+
+        return Math.Round(Math.Sqrt(x), 2);
+    }
+} 
+    /* static void Main(string[] args)
+    {
+        double[] array = {3};
+
+        Console.WriteLine("The magnitude of the given vector array is : {0}", VectorMath.Magnitude(array));
+    } 
+} */
+
+
+
