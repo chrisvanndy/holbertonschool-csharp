532a46de443f0dc3d63b8bf20253ba20ca273bc4
Chris Vanndy <2736@holbertonschool.com>
2021-08-01 11:07:30 -0500

    renamed files no ".txt" extention necessary

---
+++ 0x00-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+    class VectorMath
+    {
+        public static double[] Multiply(double[] vector, double scalar)
+        {
+            double[] product = new double[] {-1};
+
+            if (vector.Length < 2 || vector.Length > 3)
+            {
+                return product;
+            }
+
+            if (vector.Length == 2)
+            {
+                product = new double[2] (scalar * vector[0], scalar * vector[1])
+            }
+            
+            return vector;
+        }
+
+        static void Main(string[] args)
+        {
+            double[] vectorEx = new double[]{2, -3, 0};
+            double scalarEx = 2;
+
+            Console.WriteLine("{0}", string.Join(", ", VectorMath.Multiply(vectorEx, scalarEx)));
+
+        }
+    }
