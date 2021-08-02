41b6bc9c38be4c05404d3df0862d72d46e574908
Chris Vanndy <2736@holbertonschool.com>
2021-08-02 12:59:55 -0500

    testing for 16

---
+++ 0x00-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,29 @@
+﻿using System;
+///<summary>MatrixMath contains functions that performat arithmetic operations on 2D and 3D matrices.</summary>
+class MatrixMath
+{
+    ///<summary>MultiplyScalar performs matrix multiplication on a matrix (2D or 3D).</summary>
+    ///<param name="matrix1">passed parameter matrix1 can be 2D or 3D matrix of == length to matrix2.</param>
+    ///<param name="scalar">passed parameter scalar is a double by which to multiply members of the matrix.</param>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        double[,] notOK = new double[,] {{-1}};
+        int row = matrix.GetLength(0);
+        int col = matrix.GetLength(1);
+
+        if (row < 1 || col < 2)
+        {
+            return notOK;
+        }
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                matrix[i, j] = matrix[i,j] * scalar;
+            }
+        }
+        return matrix;
+
+    }
+}
+
