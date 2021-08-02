893e4bbd6e0ef249c8520329b70a485f36928da7
Chris Vanndy <2736@holbertonschool.com>
2021-08-02 10:03:44 -0500

    pushing task 12 - dotprouct

---
+++ 0x00-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+///<summary>Vector math contains functions that perform linear alebraic equations with 2D and 3D vectors.</summary>
+class VectorMath
+{
+    ///<summary>DotProduct returns the dot product of two vectors (2D or 3D) and returns the resulting double.</summary>
+    ///<param name="vector1">2D or 3D vector a</param>
+    ///<param name="vector2">2D or 3D vector b</param>
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        double dProduct = -1;
+
+        if (vector1.Length == 2 && vector1.Length == vector2.Length)
+        {
+            dProduct = (vector1[0] * vector2[0]) + (vector1[1] * vector2[1]);
+        }
+        else if (vector1.Length == 3 && vector1.Length == vector2.Length)
+        {
+            dProduct = (vector1[0] * vector2[0]) + (vector1[1] * vector2[1]) + (vector1[2] * vector2[2]);
+        }
+        return (dProduct);
+
+    }
+
+    static void Main(string[] args)
+    {
+        double[] array1 = new double[] {1, 2, 1};
+        double[] array2 = new double[] {1, 2, 2};
+
+        Console.WriteLine("[{0}]", string.Join(", ", VectorMath.DotProduct(array1, array2)));
+
+    }
+
+
+}
