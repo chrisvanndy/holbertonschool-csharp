0954a86b52a5c2e9f0a2655d183ef5802d7b97dc
Chris Vanndy <2736@holbertonschool.com>
2021-08-02 21:12:51 -0500

    push and pray for 25

---
+++ 0x00-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+///<summary>MatrixMath performs operations on matrices</summary>
+class MatrixMath
+{
+    ///<summary>Transpose flip flops the colums and rows in a matrix</summary>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        int row = matrix.GetLength(0);
+        int col = matrix.GetLength(1);
+        double[,] newMatrix = new double[column, row];
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                newMatrix[j, i] = matrix[i, j];
+            }
+        }
+        
+        return newMatrix;
+    }
+}
+
