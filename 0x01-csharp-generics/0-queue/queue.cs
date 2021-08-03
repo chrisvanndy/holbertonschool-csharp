8a0ac77dc95a28e7c75924814e36c2352fb85005
Chris Vanndy <2736@holbertonschool.com>
2021-08-03 10:38:36 -0500

    pushing task 0: first commit

---
+++ 0x01-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+///<summary>Class Queue manually defines a class which expectes a Type.</summary>
+class Queue<T>
+{
+    ///<summary>CheckType returns the type of the queue object its called on.</summary>
+    ///<return>The CheckType method returns the type of Queue object its operating on.</return>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
+
