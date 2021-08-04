ec4ac98ae2fe97c157d9b029c154c4ab775f0efd
Chris Vanndy <2736@holbertonschool.com>
2021-08-04 11:10:02 -0500

    new Class / inheritance combo

---
+++ 0x02-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+///<summary>Base, an abstract class, defines properties and methods for Base class</summary>
+abstract class Base
+{
+    public string name { get; set; }
+
+    ///<summary>ToString (overridden) returns informations about the object instance being operated on.</summary>
+    public override string ToString()
+    {
+        return (this.name + " is a " + this.GetType().ToString());
+    }
+}
+
+///<summary>IInteractive is a public interface</summary>
+interface IInteractive
+{
+    void Interact();
+}
+
+///<summary>IBreakable is a public interface</summary>
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+///<summary>ICollectable is a public interface</summary>
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+///<summary>TestObject inherits from Base, and all interfaces.</summary>
+
+///<summary>Door class inherits from Base and IInteractive</summary>
+class Door : Base, IInteractive
+{
+    //constructor provides a default vaule if one is not given
+    public Door(string def = "Door")
+    {
+        name = def;
+    }
+    ///<summary>Interact prints a string that describes interaction with object</summary>
+    public void Interact()
+    {
+        Console.WriteLine("You try to open the {0}. It's locked.", name);
+    }
+}
