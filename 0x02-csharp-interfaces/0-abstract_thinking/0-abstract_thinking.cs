a2a74980e29fa45b22f7251847a42346e57caab4
Chris Vanndy <2736@holbertonschool.com>
2021-08-04 10:04:17 -0500

    first commit

---
+++ 0x02-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+///<summary>Base, an abstract class, defines properties and methods for Base class</summary>
+abstract class Base
+{
+    public string name = "";
+
+    ///<summary>ToString (overridden) returns informations about the object instance being operated on.</summary>
+    public override string ToString()
+    {
+        return (this.name + " is a " + this.GetType().ToString());
+    }
+}
+
