17cfe875d0cbfeb48604b908a5cdad2d5e0b8fab
Chris Vanndy <2736@holbertonschool.com>
2021-08-04 10:51:37 -0500

    define 3 Interfaces + new class that inherits all

---
+++ 0x02-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+///<summary>Base, an abstract class, defines properties and methods for Base class</summary>
+abstract class Base
+{
+    public string name { get; set; }
+
+    ///<summary>ToString (overridden) returns informations about the object instance being operated on.</summary>
+    public override string ToString()
+    {
+        return (this.name + " is a " + this.GetType().ToString());
+    }
+}
+
+///<summary>IInteractive is a public interface</summary>
+interface IInteractive
+{
+    void Interact();
+}
+
+///<summary>IBreakable is a public interface</summary>
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+///<summary>ICollectable is a public interface</summary>
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+///<summary>TestObject inherits from Base, and all interfaces.</summary>
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+     ///<summary>Collect method</summary>
+    public void Collect()
+    {
+
+    }
+
+    ///<summary>Break method</summary>
+    public void Break()
+    {
+
+    }
+
+    ///<summary>Interact method</summary>
+    public void Interact()
+    {
+
+    }
+}
+
+
+
